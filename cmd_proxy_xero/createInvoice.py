a5874080e4a990c9421b063acfa44958d3d4b308
Jon Herron <jon.herron@yahoo.com>
2022-09-09 13:40:16 -0400

    Example

---
+++ cmd_proxy_xero/createInvoice.py
@@ -0,0 +1,8 @@
+def create_invoice(
+        client_id: str, 
+        client_secret: str, 
+        access_token: str, 
+        tenant_id: str,
+        due_date: str):
+    """Creates an invoice in xero."""
+    return 'todo: create invoice in xero'
